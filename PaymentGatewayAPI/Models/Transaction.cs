10412a1bfdf575ffeac83ab3b02140930c38cf64
Rohit Chaudhary <rohit.chaudhary@imark.com.np>
2025-02-03 10:18:49 +0545

    feat: Added all the required model class.

---
+++ PaymentGatewayAPI/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace PaymentGatewayAPI.Models;
+
+public class Transaction
+{
+    public Guid TransactionId { get; set; }
+    public Guid CustomerId { get; set; }
+    public decimal Amount { get; set; }
+    public string Currency { get; set; } = string.Empty;
+    public string PaymentMode { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; }
+    public required Customer Customer { get; set; }
+}
---
+++ PaymentGatewayAPI/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace PaymentGatewayAPI.Models;
+
+public class User
+{
+    public Guid UserId { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; set; }
+    public Customer? Customer { get; set; }
+}
