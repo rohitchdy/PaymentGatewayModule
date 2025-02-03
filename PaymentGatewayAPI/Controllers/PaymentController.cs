1926ec4e2f53d4413ed77aab7999749eb2c55668
Rohit Chaudhary <rohit.chaudhary@imark.com.np>
2025-02-03 10:30:26 +0545

    refactor: Rename existing controller name and remove unused model.

---
+++ PaymentGatewayAPI/Controllers/PaymentController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentGatewayAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+    }
+}
